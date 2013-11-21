97dd3d106ca58d526a4b744b73cd314b5623c19c
Greg Cerveny <greg.cerveny@gmail.com>
2013-11-21 13:54:17 -0600

    adding mongodb connection

--- nickslist/app.js
+++ nickslist/app.js
@@ -1,30 +1,34 @@
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
+  , MongoClient = require('mongodb').MongoClient
   , path = require('path');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+MongoClient.connect('mongodb://localhost:27017/blog', function(err, db) {
+    if(err) throw err;
 
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', routes.index);
-app.get('/category/:category', routes.category);
-app.get('/listing', routes.listing);
-app.post('/listing', routes.addListing);
+    if ('development' == app.get('env')) {
+        app.use(express.errorHandler());
+    }
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+    app.get('/', routes.index(db));
+    app.get('/category/:category', routes.category(db));
+    app.get('/listing', routes.listing(db));
+    app.post('/listing', routes.addListing(db));
+
+    http.createServer(app).listen(app.get('port'), function(){
+        console.log('Express server listening on port ' + app.get('port'));
+    });
 });
--- nickslist/routes/index.js
+++ nickslist/routes/index.js
@@ -1,24 +1,31 @@
-var collection = {jobs:[], events:[], 'for sale':[]};
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Nicks List', categories: Object.keys(collection) });
+exports.index = function(db) {
+    return function(req, res){
+        res.render('index', { title: 'Nicks List', categories: [] });
+    };
 };
 
-exports.category = function(req, res){
-  var category = req.params.category;
-  res.render('category', { title: req.params.category, listings: collection[category] });
+exports.category = function(db) {
+    return function(req, res){
+        var category = req.params.category;
+        res.render('category', { title: req.params.category, listings: [] });
+    };
+}
+
+exports.addListing = function(db) {
+    return function(req, res){
+        collection[req.body.category].push(
+            {title: req.body.title,
+             user: req.body.user,
+             description: req.body.description}
+        );
+        res.send ("listing added");
+    };
 };
 
-exports.addListing = function(req, res){
-  collection[req.body.category].push(
-      {title: req.body.title,
-      user: req.body.user,
-      description: req.body.description}
-  );
-  res.send ("listing added");
-};
-
-exports.listing = function(req, res){
-  res.render('listing', {title: "New Listing"});
+exports.listing = function(db) {
+    return function(req, res){
+        res.render('listing', {title: "New Listing"});
+    };
 };
 
